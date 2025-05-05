81d40cd2b868fd1a574f143650524e35fb902fde
taylorc <taylorc5150@gmail.com>
2025-05-05 18:56:09 +1000

    added nuxt ui

--- src/WebApp/app/app.vue
+++ src/WebApp/app/app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
-    <WeatherForecasts />
-  </div>
+   <UApp>
+    <NuxtPage />
+  </UApp>
 </template>
---
+++ src/WebApp/app/pages/forecasts.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <WeatherForecasts />
+  </div>
+</template>
