1b1ff2afb4507f40942f9c7565461f5810e273f5
taylorc <taylorc5150@gmail.com>
2025-05-06 10:30:12 +1000

    removed nuxtui. added tailwindcss and daisyui

--- src/WebApp/app/pages/index.vue
+++ src/WebApp/app/pages/index.vue
@@ -1,5 +1,17 @@
 <template>
-  <div>
-    <h1>Welcome to the Index Page</h1>
+  <h1 class="text-3xl font-bold underline">
+    Welcome to the Index Page
+    <button class="btn btn-secondary">Secondary</button>
+    
+  </h1>
+  <div class="chat chat-start">
+  <div class="chat-bubble">
+    It's over Anakin,
+    <br />
+    I have the high ground.
   </div>
+</div>
+<div class="chat chat-end">
+  <div class="chat-bubble">You underestimate my power!</div>
+</div>
 </template>
--- src/WebApp/nuxt.config.ts
+++ src/WebApp/nuxt.config.ts
@@ -1,11 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  modules: ['@nuxt/ui'],
+  modules: [],
   css: ['~/assets/css/main.css'],
   devtools: { enabled: true },
   future: {
     compatibilityVersion: 4
+  },
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
   },
   $development: {
     routeRules: {
